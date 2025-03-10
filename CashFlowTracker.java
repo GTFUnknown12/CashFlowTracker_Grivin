6d3a6f4323199cc6995b0c4ddac28f12a16965ad
GTFUnknown12 <gr1vin.gamer@gmail.com>
2025-03-11 04:09:04 +0700

    Add files via upload

---
+++ CashFlowTracker.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LaporanKeuangan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CashFlowTracker {
+    // Attributes
+    private double TotalPemasukan;
+    private double saldo;
+    private double TotalPengeluaran;
+    private String NamaPemilik;
+    private String MataUang;
+
+    // Constructor
+    public CashFlowTracker(String namaPemilik, String mataUang) {
+        this.NamaPemilik = namaPemilik;
+        this.MataUang = mataUang;
+        this.TotalPemasukan = 0.0;
+        this.TotalPengeluaran = 0.0;
+        this.saldo = 0.0;
+    }
+
+    // Method to add a transaction
+    public void tambahTransaksi(String deskripsi, double jumlah, boolean adalahPemasukan) {
+        if (adalahPemasukan) {
+            TotalPemasukan += jumlah;
+            saldo += jumlah;
+        } else {
+            TotalPengeluaran += jumlah;
+            saldo -= jumlah;
+        }
+        // Logic to store transaction details can be added here
+    }
+
+    // Method to display transactions
+    public void tampilkanTransaksi() {
+        // Logic to display transactions can be added here
+        System.out.println("Menampilkan transaksi...");
+    }
+
+    // Method to search for a transaction
+    public void cariTransaksi(String kataKunci) {
+        // Logic to search for a transaction can be added here
+        System.out.println("Mencari transaksi dengan kata kunci: " + kataKunci);
+    }
+
+    // Method to delete a transaction
+    public void hapusTransaksi(int indek) {
+        // Logic to delete a transaction can be added here
+        System.out.println("Menghapus transaksi pada indeks: " + indek);
+    }
+
+    // Method to get a report
+    public void dapatkanLaporan() {
+        // Logic to generate a report can be added here
+        System.out.println("Mendapatkan laporan...");
+        System.out.println("Total Pemasukan: " + TotalPemasukan + " " + MataUang);
+        System.out.println("Total Pengeluaran: " + TotalPengeluaran + " " + MataUang);
+        System.out.println("Saldo: " + saldo + " " + MataUang);
+    }
+}
